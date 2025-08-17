67796e26a3872ecbdc1dfad9d99acc78573fb2f3
lamaachi <istatasila7@gmail.com>
2025-08-18 00:07:27 +0100

    Init front and back

---
+++ Backend/api/src/test/java/com/docmind/api/DocmindApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.docmind.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DocmindApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
